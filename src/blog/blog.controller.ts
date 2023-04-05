c0ba0f97ca89dc6168b3277b080352103a5bc291
aom19 <39848284+aom19@users.noreply.github.com>
2023-04-05 22:57:24 +0300

    service functions

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,22 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Request } from '@nestjs/common';
 import { BlogService } from './blog.service';
-import { Get, Post, Patch, Delete } from '@nestjs/common';
+import { Get, Post, Patch, Delete, Param } from '@nestjs/common';
+
+import { BlogEntry } from './models/blog-entry.interface';
+import { Observable } from 'rxjs';
 
 @Controller('blog')
 export class BlogController {
   constructor(private blogService: BlogService) {}
 
   @Get()
-  getAllBlogs() {}
+  getAllBlogs(): Observable<BlogEntry[]> {
+    return this.blogService.getAllBlogs();
+  }
 
   @Get(':id')
-  getBlogById() {}
+  getBlogById(@Param('id') id: number): Observable<BlogEntry> {
+    return this.blogService.getBlogById(id);
+  }
 
-  @Get('author/:id')
-  getBlogByAuthorId() {}
+  @Get('author/:authorId')
+  getBlogByAuthorId(
+    @Param('authorId') authorId: number,
+  ): Observable<BlogEntry[]> {
+    return this.blogService.getBlogByAuthorId(authorId);
+  }
 
   @Post()
-  createBlog() {}
+  createBlog(@Body() blog: BlogEntry, @Request() req): Observable<BlogEntry> {
+    const { user } = req.user;
+    return this.blogService.createBlog(user, blog);
+  }
 
   @Patch(':id')
   updateBlog() {}
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BlogEntryEntity } from './models/blog-entry.entity';
+import { Observable, map, from } from 'rxjs';
+import { BlogEntry } from './models/blog-entry.interface';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+  constructor(
+    @InjectRepository(BlogEntryEntity)
+    private readonly blogRepository: Repository<BlogEntryEntity>,
+    private readonly userService: UserService,
+  ) {}
+
+  getAllBlogs(): Observable<BlogEntryEntity[]> {
+    return from(this.blogRepository.find());
+  }
+
+  getBlogById(id: number): Observable<BlogEntryEntity> {
+    return from(this.blogRepository.findOne({ where: { id } }));
+  }
+
+  getBlogByAuthorId(authorId: number): Observable<BlogEntryEntity[]> {
+    return from(this.blogRepository.find());
+  }
+
+  createBlog(blogEntry: BlogEntryEntity, user: User): Observable<BlogEntry> {
+    return from(this.blogRepository.save(blogEntry));
+  }
+
+  updateBlog(id: number, blogEntry: BlogEntryEntity): Observable<any> {
+    return from(this.blogRepository.update(id, blogEntry));
+  }
+
+  deleteBlog(id: number): Observable<any> {
+    return from(this.blogRepository.delete(id));
+  }
+}
